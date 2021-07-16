829e510c9dcd18589a9743b57b5351e3a444b6b4
Joroh <mohamedaitsimhand@gmail.com>
2021-07-16 18:28:13 +0100

    Module00/ex00

---
+++ Module00/ex00/megaphone.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+	if (argc > 1)
+	{
+		for (int i = 1; i < argc; i++)
+			for (int j = 0; j < (int)strlen(argv[i]); j++)
+				putchar(toupper(argv[i][j]));
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << std::endl;
+	return 0;
+}
